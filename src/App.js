e94f8330b10e733f38ebdc2597a42fcb2c2bdc21
ampsteric <ampsteric@gmail.com>
2020-05-18 11:25:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrReader from 'react-qr-reader'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    result: 'No result'
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div className="App">
+              <div className="header">
+              <h1>QRery</h1>
+              </div>
+
+
+              <div className="container">
+
+                              <div className="left">
+                                <QrReader
+                                    delay={300}
+                                    onError={this.handleError}
+                                    onScan={this.handleScan}
+                                    style={{ width: '100%',
+                                            }}
+                                  />
+                              </div>
+                    
+
+                            <div className="right">
+                                    <img src={require('./gif.gif')} alt="qrscan" /> 
+
+                            </div>
+
+
+
+              </div>
+                                         <div> <p><span>Decoded Text: </span>{this.state.result}</p></div>
+
+             
+       </div>
+    )
+  }
 }
-
 export default App;
+
+
